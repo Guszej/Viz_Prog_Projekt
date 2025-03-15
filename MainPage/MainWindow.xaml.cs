545880d48bfae892679ed8b623b3692b17a65c59
Guszej <104644396+Guszej@users.noreply.github.com>
2025-03-15 16:18:15 +0100

    Add files via upload
    3 gomb a main oldalon

---
+++ MainPage/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MainPage
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btLogin(object sender, RoutedEventArgs e)
+        {
+            Login loginpg = new Login();
+            loginpg.Show();
+        }
+
+        private void btGuest(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btReg(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
